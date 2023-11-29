e79dc39b312136c9f7754937a6861d7743801a33
lucie-cloud <luhaya@yahoo.com.br>
2023-11-29 11:59:24 -0300

    Commit dicionario atividade2911 alterado

--- Dicionário.py
+++ Dicionário.py
@@ -5,12 +5,14 @@
 # Apresentação
 print('\n\t\t\t -- Sistema de Informação Cadastro de Funcionários -- \n')
 
+# Estrutura
+funcionário = {'Nome': '', 'Salário':0.0, 'Ativo':False}
+
 # Entrada
 NomedoFuncionário = input ('Nome: ')
 Salário = float(input('Salário: R$ '))
-StatusdoFuncionário = bool(input('Status: True'))
+StatusdoFuncionário = bool(input ('Status: Ativo  '))
 
 # Saída
-print ("Funcionário Ativo") if funcionário ['Ativo'] else print ("Funcionário Inativo")
-thisdict = dict (Nome = "" , Salário = "" , Status = "" )
-print (thisdict)
+#print ("Funcionário Ativo") if Status ['Ativo'] else print ("Funcionário Inativo")
+print("*** Funcionário Ativo  ***") if StatusdoFuncionário ['True'] else print ("*** Funcionário Inativo ***")
