55351f461ac7e5b8c2c1ad391003f051a31fb29d
lucie-cloud <luhaya@yahoo.com.br>
2023-11-29 11:15:19 -0300

    Commit dicionario atividade2911

---
+++ Dicionário.py
@@ -0,0 +1,16 @@
+# Dicionário
+
+# Sistema de Informação - Cadastrar funcionário nome,salario
+
+# Apresentação
+print('\n\t\t\t -- Sistema de Informação Cadastro de Funcionários -- \n')
+
+# Entrada
+NomedoFuncionário = input ('Nome: ')
+Salário = float(input('Salário: R$ '))
+StatusdoFuncionário = bool(input('Status: True'))
+
+# Saída
+print ("Funcionário Ativo") if funcionário ['Ativo'] else print ("Funcionário Inativo")
+thisdict = dict (Nome = "" , Salário = "" , Status = "" )
+print (thisdict)
